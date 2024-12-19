454e619092ad56392ded12452b567a03b2d859ee
nergalex <nergalex@hotmail.com>
2024-12-19 17:23:58 +0100

    mv directory

--- debian/nginx-unprivileged-r33_agent/entrypoint.sh
+++ debian/nginx-unprivileged-r33_agent/entrypoint.sh
@@ -2,10 +2,10 @@
 #
 # This script launches nginx and nginx-agent.
 #
-echo "------ version 2024.12.19.01 ------"
+echo "------ version 2024.12.19.02 ------"
 
 install_path="/nginx"
-mv -f /nginx-initial-config/ ${install_path}/etc/nginx/
+mv -f /nginx-initial-config/* ${install_path}/etc/nginx/*
 
 handle_term()
 {
