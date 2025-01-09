42283d3e028daf8e6d8272ea0483db0017676f3d
nergalex <nergalex@hotmail.com>
2025-01-09 10:02:47 +0100

    default nginx.conf

--- debian/nginx-unprivileged-r33_agent/entrypoint.sh
+++ debian/nginx-unprivileged-r33_agent/entrypoint.sh
@@ -2,12 +2,12 @@
 #
 # This script launches nginx and nginx-agent.
 #
-echo "------ version 2025.01.09.01 ------"
+echo "------ version 2025.01.09.02 ------"
 
 install_path="/nginx"
 
 # copy initial file to the empy volume, in case of being empty
-cp -pf /nginx-initial-config/* ${install_path}/etc/nginx/
+cp -prf /nginx-initial-config ${install_path}/etc/nginx
 
 handle_term()
 {
